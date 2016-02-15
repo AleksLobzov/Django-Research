4c41159b2950a08484193fc5a0bcae4ef29944e1
Alexey Lobzov <aleks.lobzov@gmail.com>
2016-02-15 22:36:53 +0300

    Added autotests for objects naming and one highchart on the main page

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Author, Book, Publisher
+
+
+class AuthorMethodTests(TestCase):
+
+  
+  def test_author_name(self):
+    '''
+    Author.__str__() should return correct string with author’s first and last names
+    '''
+    a = Author(salutation='Sr.',
+               first_name='John',
+               last_name='Smith',
+               email='john.smith@gmail.com')
+    self.assertEqual(str(a), 'John Smith')
+
+
+  def test_book_title(self):
+    '''
+    Book.__str__() should return correct string with book title
+    '''
+    b = Book(title='Marina')
+    self.assertEqual(str(b), 'Marina')
+
+
+  def test_publisher_name(self):
+    '''
+    Publisher.__str__() should return correct string with publisher name
+    '''
+    p = Publisher(name='Mann, Ivanov, Ferber')
+    self.assertEqual(str(p), 'Mann, Ivanov, Ferber')
--- books/views.py
+++ books/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Book, Author
+from .models import Book, Author, Publisher
 
 
 def index(request):
   latest_book_list = Book.objects.order_by('-publication_date')[:10]
-  context = {'latest_book_list': latest_book_list}
+  
+  book_by_publisher_piechart_data = [[str(publisher), len(Book.objects.all().filter(publisher=publisher))] for publisher in Publisher.objects.all()]
+  
+  context = {'latest_book_list': latest_book_list, 'book_by_publisher_piechart_data': book_by_publisher_piechart_data}
   return render(request, 'books/index.html', context)
 
 
@@ -18,3 +21,4 @@ def author_detail(request, author_id):
   author = get_object_or_404(Author, pk=author_id)
   books = Book.objects.all().filter(authors=author)
   return render(request, 'books/author_detail.html', {'author': author, 'books': books})
+
